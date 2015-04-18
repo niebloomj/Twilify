cc729dfd2af3a6c538cdda39e39f0e3dadda30a3
Jacob Niebloom <niebloomj@gmail.com>
2015-04-18 17:06:25 -0400

    starting php

--- smsResponse.php
+++ smsResponse.php
@@ -2,6 +2,7 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
+
 <Response>
     <Message>Normal or Party mode? Reply "Normal" or "Party"</Message>
 </Response>
--- smstest.php
+++ smstest.php
@@ -1,16 +1,25 @@
 <?php
-// Install the library via PEAR or download the .zip file to your project folder.
-// This line loads the library
 require('twilio/Services/Twilio.php');
 
-$sid = "AC57903eef4f1064c36f05f84b2dcff21d"; // Your Account SID from www.twilio.com/user/account
-$token = "c799f3bda46a6ab81188f3200f3d9827"; // Your Auth Token from www.twilio.com/user/account
+$sid = "AC57903eef4f1064c36f05f84b2dcff21d";
+$token = "c799f3bda46a6ab81188f3200f3d9827";
 
 $client = new Services_Twilio($sid, $token);
-$message = $client->account->messages->sendMessage(
-  '5852095561', // From a valid Twilio number
-  '9733095897', // Text this number
-  "Hello!"
-);
 
-print $message->sid;
+function sendMsgToAdmin() {
+	$message = $client->account->messages->sendMessage(
+	'5852095561',
+	'9733095897',
+	"Hello!");
+	echo $message->sid;
+}
+
+$messages = $client->account->messages->getIterator(0, 50, array( 
+	'To' => "+15852095561",  
+)); 
+ 
+foreach ($messages as $message) { 
+	echo $message->body, PHP_EOL, "from", $message->from; 
+}
+
+?>
