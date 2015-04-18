f30e0bad61045a1f09a20e6929aa3c81030d7fb5
Jacob Niebloom <niebloomj@gmail.com>
2015-04-18 16:48:20 -0400

    party or normal

--- smsResponse.php
+++ smsResponse.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hello there</Message>
+    <Message>Normal or Party mode? Reply "Normal" or "Party"</Message>
 </Response>
