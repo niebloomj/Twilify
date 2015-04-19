2f7cbaee4221512c13e6fbea5a9207c6ac67bcca
Manan Hora <horamanan@gmail.com>
2015-04-19 12:50:12 -0400

    Jacob

--- js/uris.js
+++ js/uris.js
@@ -1,6 +1,6 @@
 uris = [];
 musiccue = [];
-var mlength = 0;
+var numOfInvisibleMessages = 0;
 uris.push("blah");
 uris.push("https://embed.spotify.com/?uri=spotify%3Atrack%3A2CJtimCSGAn8x6RE3irZFV");
 uris.push("https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UGQt");
@@ -10,68 +10,68 @@ uris.push("https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UG
 uris.push("https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UGQt");
 uris.push("https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UGQt");
 
-function song(trackname, url){
-	this.trackname = trackname;
-	this.url = url;
+function song(trackname, url) {
+    this.trackname = trackname;
+    this.url = url;
 }
 
-function createSong(trackname,list){
-	list.push(trackname);
-	console.log(list);
-	return list;
-}
-// createSong("wake me up", musiccue );
-// createSong("shake you down", musiccue );
-// createSong("sugar", musiccue );
-// createSong("moves like jagger", musiccue );
+function createSong(trackname, list) {
+        list.push(trackname);
+        console.log(list);
+        return list;
+    }
+    // createSong("wake me up", musiccue );
+    // createSong("shake you down", musiccue );
+    // createSong("sugar", musiccue );
+    // createSong("moves like jagger", musiccue );
 
-function getMessages(){
-        var messages = $("#textMessages li");
-        var j = 0;
-        for(var i = messages.length-1; i>=mlength;i--){
+function getMessages() {
+    var messages = $("#textMessages li");
+    var j = 0;
+    if (numOfInvisibleMessages == 0) {
+        for (var i = messages.length - 1; i >= numOfInvisibleMessages; i--) {
 
-        var n = messages[i].innerHTML.search(" ");
-        var x = messages[i].innerHTML;
+            var n = messages[i].innerHTML.search(" ");
 
-        musiccue[j] = messages[i].innerHTML.substring(n+1, messages[i].length);
+            musiccue[j] = messages[i - numOfInvisibleMessages].innerHTML.substring(n + 1, messages[i - numOfInvisibleMessages].length);
+            console.log("j " + j + " " + musiccue[j]);
 
-        j++;
+            j++;
         }
-        console.log(musiccue);
-        mlength=messages.length;
-                populate();
-                console.log(musiccue[musiccue.length-1]);
-
+    } else {
+        j = 0
+        for (var i = 0; i < numOfInvisibleMessages - rem; i++) {
+            var n = messages[i].innerHTML.search(" ");
+            musiccue[j] = messages[i].innerHTML.substring(n + 1, messages[i].length);
+            j++
         }
+    }
+    console.log(musiccue);
+    populate();
+    numOfInvisibleMessages = messages.length;
 
-        function vsearch(array, value){
-                var bool = false;
-                for(var a = 0; a<array.length;a++){
-                        if(array[a]===value){
-                                bool = true;
-                        }
-                }
-                return bool;
-        }
-
-function populate(){
-        var l = $('#myList li');
-for ( var i=l.length,  len = musiccue.length; i<len; i++) { 
-        //define the listItem, i.e. a new list element
-
-      
-      
-        var listItem = document.createElement("li");
-        listItem.innerHTML = musiccue[i];
-        
-        //locate where the item is to be added in the DOM and add it so that it displays in the browser
-        $('#myList').append(listItem);
-}
-
-        //$('#myorder').listview('refresh');
-        console.log("Well, it ran at least");
-
+    console.log(musiccue[musiccue.length - 1]);
 
 }
+var currentListLength = 0;
+var didUpdate = false;
 
-// populate();
+function populate() {
+    var myList = document.getElementById("myList");
+    if (musiccue.length > currentListLength - rem) {
+        // currentListLength = 0
+        myList.innerHTML = '';
+        for (var i = 0; i < musiccue.length; i++) {
+            currentListLength++;
+
+            var li = document.createElement("li");
+            if (!didUpdate) {
+                li.appendChild(document.createTextNode(musiccue[i]));
+            } else {
+                li.appendChild(document.createTextNode(musiccue[musiccue.length - i - 1]));
+            }
+            myList.appendChild(li);
+        }
+        didUpdate = true
+    }
+}
