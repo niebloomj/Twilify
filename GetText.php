3fb9cb608b2dc7247c6052e9d319c3dced3519c1
Jacob Niebloom <niebloomj@gmail.com>
2015-04-19 04:23:30 -0400

    fixed dah bug

--- GetText.php
+++ GetText.php
@@ -3,7 +3,9 @@ require('twilio/Services/Twilio.php');
 $sid = "AC57903eef4f1064c36f05f84b2dcff21d";
 $token = "c799f3bda46a6ab81188f3200f3d9827";
 $client = new Services_Twilio($sid, $token);
-foreach ($client->account->messages as $message) {
+foreach ($client->account->messages->getIterator(0, 50, array(
+    'To' => '+15852095561',
+)) as $message) {
 $firstWord = explode(" ", $message->body)[0];
 if ($firstWord == "23687"){echo "<li>". $message->body . "</li>";}}
 
